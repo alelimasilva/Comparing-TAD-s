934455e45747c4957c9b1accd6bcd93995a91964
JotaChina <72946706+JotaChina@users.noreply.github.com>
2020-11-18 17:14:28 -0300

    Inclui as declarações de funções pra tirar os warnings de compilação

--- arvore.h
+++ arvore.h
@@ -7,3 +7,9 @@ Registro registro;
 Apontador esq, dir;
 } No;
 typedef Apontador Dicionario;
+
+void pesquisar(Registro *x, Apontador p);
+void inserir(Registro x, Apontador *p);
+void inicializar(Apontador *dicionario);
+void retirar(Registro x, Apontador *p);
+void antecessor(Apontador q, Apontador *r);
