12f533cafc9dce970b46881bf7fb52da1ae74cbf
fferraz07 <72937158+fferraz07@users.noreply.github.com>
2020-11-19 17:21:40 -0300

    Update arvore.c

--- arvore.c
+++ arvore.c
@@ -21,7 +21,7 @@ void pesquisar(Registro *x, Apontador p){
 
 void inserir(Registro x, Apontador *p){
     if (*p == NULL){ // so insere se o que p aponta for NULL
-        *p = (Apontador)malloc(sizeof(No)); // p passa a apontar para um no
+        *p = (Apontador)malloc(sizeof(Celula)); // p passa a apontar para um no
         (*p)->registro = x; (*p)->esq = NULL; (*p)->dir = NULL; // adiciona os valores e o esq e dir dele se tornam NULL
         return;
     }
@@ -40,30 +40,30 @@ void inicializar(Apontador *arvore){
     *arvore = NULL;
 }
 
-void retirar(Registro x, Apontador *p){
-    Apontador aux;
-    if (*p == NULL) {
+void retirar(Registro x, Apontador *p){ // funcao para retirar um registro da arvore
+    Apontador aux; // ponteiro auxiliar utilizado para receber o valor o ponteiro *p passado na funcao
+    if (*p == NULL) { // checando se o registro esta presente na arvore
         printf("Erro : Registro nao esta na arvore\n");
-    } else if (x.chave < (*p)->registro.chave){
+    } else if (x.chave < (*p)->registro.chave){ // caso a chave seja menor que o registro a funcao é chamada novamente com os valores de x e com o ponteiro *p apontando pra sua esquerda
         retirar(x, &(*p)->esq); return;
-    } else if (x.chave > (*p)->registro.chave){
+    } else if (x.chave > (*p)->registro.chave){ // caso a chave seja maior que o registro a funcao é chamada novamente com os valores de x e com o ponteiro *p apontando pra sua direita
         retirar(x, &(*p)->dir); return;
-    } else if ((*p)->dir == NULL){
+    } else if ((*p)->dir == NULL){ // verificando se a direita do ponteiro esta vazia, se estiver, o auxiliar recebe *p e *p aponta para a esquerda do ponteiro, liberando a memoria do auxiliar apos o processo 
         aux = *p;
         *p = (*p)->esq;
         free(aux);
-    }else if ((*p)->esq == NULL){
+    }else if ((*p)->esq == NULL){ // verificando se a esquerda do ponteiro esta vazia, se estiver, o auxiliar recebe *p e *p aponta para a direita do ponteiro, liberando a memoria do auxiliar apos o processo 
         aux = *p;
         *p = (*p)->dir;
         free(aux);
     }
     else{
-        antecessor(*p, &(*p)->esq);
+        antecessor(*p, &(*p)->esq); // funcao chamada para organizar a arvore apos a retirada do registro
     }
 }
 
-void antecessor(Apontador q, Apontador *r){
-    if ((*r)->dir != NULL) {
+void antecessor(Apontador q, Apontador *r){ // funcao que recebe 2 ponteiros e verifica se a direita esta vazia e caso estiver vazia a ligacao entre os nos é feita 
+    if ((*r)->dir != NULL) { // caso nao esteja vazia, a funcao é chamada novamente com o segundo ponteiro apontando para sua direita
         antecessor(q, &(*r)->dir);
         return;
     } else{
@@ -73,4 +73,5 @@ void antecessor(Apontador q, Apontador *r){
         free(q);
     }
 }
+
 
