ef82fac3978f248a0ca86d54408d64e6a42ddfff
JotaChina <72946706+JotaChina@users.noreply.github.com>
2020-11-27 19:41:34 -0300

    atualizando main de testes
    hash pronta

--- main.c
+++ main.c
@@ -11,38 +11,52 @@ int x, y; 					   //variaveis de suporte para os loops do menu
 
 do{
 	limpaTela();
-	printf("Insira 1 pra adicionar valores a Hash\n"
-		   "Insira 2 para ver a quantidade de itens na HASH\n"
-	       "Insira 3 para buscar uma chave na HASH\n"
-		   "Insira 0 para sair\n");
+	printf("|1 adicionar valores a HASH\n"
+		   "|2 buscar uma chave na HASH\n"
+	       "|3 remover uma chave na HASH\n"
+		   "|4 quantidade de itens na HASH\n"
+		   "|0 sair\n"
+		   "|Opcao escolhida: ");
 	scanf("%d", &x);
 	switch(x){
-		case 1:
-			limpaTela();
+		case 1:			
 			do{
-			printf("Insira o valor da chave: ");
-			scanf("%d", &y);
-			insere_hash(tabela, d, y);
-			printf("0 para voltar: ");
-			scanf("%d", &y);
+				limpaTela();
+				printf("|Insira o valor da chave: ");
+				scanf("%d", &y);
+				insere_hash(tabela, d, y);
+				printf("|0 voltar: ");
+				scanf("%d", &y);
 			}while(y != 0);
 			break;
 		case 2:
-			limpaTela();
 			do{
-			printf("A quantidade eh: %d\n", tabela->quantidade);
-			printf("0 para voltar: ");
-			scanf("%d", &y);
+				limpaTela();
+				printf("|Insira o valor da chave a ser buscada: ");
+				scanf("%d", &y);
+				busca_hash(tabela, y, d);
+				printf("|0 voltar: ");
+				scanf("%d", &y);
 			}while(y != 0);
 			break;
 		case 3:
-			limpaTela();
 			do{
-			printf("Insira o valor da chave a ser buscada: ");
-			scanf("%d", &y);
-			busca_hash(tabela, y, d);
-			printf("0 para voltar: ");
-			scanf("%d", &y);
+				limpaTela();
+				limpaTela();
+				printf("|Insira o valor da chave a ser buscada: ");
+				scanf("%d", &y);
+				retira_hash(tabela, y, d);
+				
+				printf("|0 voltar: ");
+				scanf("%d", &y);
+			}while(y != 0);
+			break;
+		case 4: 
+			do{
+				limpaTela();
+				printf("|A quantidade eh: %d\n", tabela->quantidade);
+				printf("|0 voltar: ");
+				scanf("%d", &y);
 			}while(y != 0);
 			break;
 		default:			
