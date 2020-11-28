5f6d7f8cd7964368b8368ded487ab51cc8be85c9
fferraz07 <72937158+fferraz07@users.noreply.github.com>
2020-11-28 09:55:58 -0300

    Update hashLiEnc.h

--- hashLiEnc.h
+++ hashLiEnc.h
@@ -11,13 +11,13 @@ struct registro {
 	int chave; 
 	struct registro *prox; 
 };
-typedef struct registro No; // Nó da hash
+typedef struct registro Celula; // Nó da hash
 
 void limpaTela();//Limpa o terminal para melhorar a aparencia do programa
-void inicializa_hash(No **hash); // inicializa a hash
+void inicializa_hash(Celula **hash); // inicializa a hash
 int calcula_posicao(int chave); // calcula a posição da hash
-void insere_hash(int chave, No **hash); // verifica se pode ser inserido, se sim insere a chave na hash
-void imprime_hash(No **hash); // imprime toda a hash
-void remove_hash(int chave, No **hash); // apaga um dado da hash
-void imprime_listaenc(int pos, No **hash); // percorre uma lista encadeada em uma posição da hash e a imprime
-int buscalista(int chave, No **hash); // busca um dado em uma lista encadeada
+void insere_hash(int chave, Celula **hash); // verifica se pode ser inserido, se sim insere a chave na hash
+void imprime_hash(Celula **hash); // imprime toda a hash
+void remove_hash(int chave, Celula **hash); // apaga um dado da hash
+void imprime_listaenc(int pos, Celula **hash); // percorre uma lista encadeada em uma posição da hash e a imprime
+int buscalista(int chave, Celula **hash); // busca um dado em uma lista encadeada
