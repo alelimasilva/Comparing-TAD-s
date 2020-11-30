1ba4090d96d22dff3976d16f13de3ee52973dcab
fferraz07 <72937158+fferraz07@users.noreply.github.com>
2020-11-30 19:08:35 -0300

    Add files via upload

--- arvore.c
+++ arvore.c
@@ -2,34 +2,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void pesquisar(Registro *x, Apontador p){
+void pesquisar(int chave, Apontador p){
     if (p == NULL){ //condição de saida da recursao caso nao seja encontrado o valor
         printf("Erro: Registro nao esta presente na arvore\n");
         return;
     }
-    if (x->chave < p->registro.chave){ //analisa se a chave do registro a ser procurado é menor do que a chave do no
-        pesquisar(x, p->esq); // procura novamente no nó filho a esquerda
+    if (chave < p->chave){ //analisa se a chave do registro a ser procurado é menor do que a chave do no
+        pesquisar(chave, p->esq); // procura novamente no nó filho a esquerda
     }
-    else if (x->chave > p->registro.chave){  //analisa se a chave do registro a ser procurado é maior do que a chave do no
-        pesquisar(x, p->dir); // procura novamente no nó filho a direita
+    else if (chave > p->chave){  //analisa se a chave do registro a ser procurado é maior do que a chave do no
+        pesquisar(chave, p->dir); // procura novamente no nó filho a direita
     }
     else{// se o valor for encontrado
         printf("Registro encontrado na arvore\n");
-        *x = p->registro; // o registro x recebe o registro da nó encontrado
+        //*x = p->chave; // o registro x recebe o registro da nó encontrado
     }
 }
 
-void inserir(Registro x, Apontador *p){
+void inserir(int chave, Apontador *p){
     if (*p == NULL){ // so insere se o que p aponta for NULL
         *p = (Apontador)malloc(sizeof(Celula)); // p passa a apontar para um no
-        (*p)->registro = x; (*p)->esq = NULL; (*p)->dir = NULL; // adiciona os valores e o esq e dir dele se tornam NULL
+        (*p)->chave = chave; (*p)->esq = NULL; (*p)->dir = NULL; // adiciona os valores e o esq e dir dele se tornam NULL
         return;
     }
-    if (x.chave < (*p)->registro.chave){ //analisa se a chave do registro a ser procurado é menor do que a chave do no
-        inserir(x, &(*p)->esq); return; // entao tenta inserir no nó a esq 
+    if (chave < (*p)->chave){ //analisa se a chave do registro a ser procurado é menor do que a chave do no
+        inserir(chave, &(*p)->esq); return; // entao tenta inserir no nó a esq 
     }
-    else if (x.chave > (*p)->registro.chave){ //analisa se a chave do registro a ser procurado é maior do que a chave do no
-        inserir(x, &(*p)->dir); // entao tenta inserir no nó a esq 
+    else if (chave > (*p)->chave){ //analisa se a chave do registro a ser procurado é maior do que a chave do no
+        inserir(chave, &(*p)->dir); // entao tenta inserir no nó a esq 
     }
     else{ //caso ele nao seja nem maior ou menor ele ja existe na arvore 
         printf("Erro : Registro ja existe na arvore\n");
@@ -40,14 +40,14 @@ void inicializar(Apontador *arvore){ // inicializar a arvore
     *arvore = NULL;
 }
 
-void retirar(Registro x, Apontador *p){ // funcao para retirar um registro da arvore
+void retirar(int chave, Apontador *p){ // funcao para retirar um registro da arvore
     Apontador aux; // ponteiro auxiliar utilizado para receber o valor o ponteiro *p passado na funcao
     if (*p == NULL) { // checando se o registro esta presente na arvore
         printf("Erro : Registro nao esta na arvore\n");
-    } else if (x.chave < (*p)->registro.chave){ // caso a chave seja menor que o registro a funcao é chamada novamente com os valores de x e com o ponteiro *p apontando pra sua esquerda
-        retirar(x, &(*p)->esq); return;
-    } else if (x.chave > (*p)->registro.chave){ // caso a chave seja maior que o registro a funcao é chamada novamente com os valores de x e com o ponteiro *p apontando pra sua direita
-        retirar(x, &(*p)->dir); return;
+    } else if (chave < (*p)->chave){ // caso a chave seja menor que o registro a funcao é chamada novamente com os valores de x e com o ponteiro *p apontando pra sua esquerda
+        retirar(chave, &(*p)->esq); return;
+    } else if (chave > (*p)->chave){ // caso a chave seja maior que o registro a funcao é chamada novamente com os valores de x e com o ponteiro *p apontando pra sua direita
+        retirar(chave, &(*p)->dir); return;
     } else if ((*p)->dir == NULL){ // verificando se a direita do ponteiro esta vazia, se estiver, o auxiliar recebe *p e *p aponta para a esquerda do ponteiro, liberando a memoria do auxiliar apos o processo 
         aux = *p;
         *p = (*p)->esq;
@@ -67,11 +67,9 @@ void antecessor(Apontador q, Apontador *r){ // funcao que recebe 2 ponteiros e v
         antecessor(q, &(*r)->dir);
         return;
     } else{
-        q->registro = (*r)->registro;
+        q->chave = (*r)->chave;
         q = *r;
         *r = (*r)->esq;
         free(q);
     }
 }
-
-
--- main.c
+++ main.c
@@ -1,71 +1,31 @@
-#include "hashEndAb.c" //importando biblioteca de funções
+#include "arvore.h" 
+#include <stdio.h>
+#include <stdlib.h> 
+#include <time.h>
+
+
 
 int main(){
+    clock_t t;
+    Arvore a;
+	inicializar(&a);
+
+    int vetor[TAM];
+    FILE *arquivo;
+    arquivo = fopen("teste900n.txt" , "r");
+    if(arquivo == NULL){
+        printf("Erro ao carregar a arquivo.\n");
+        exit(1);
+    }
+    for(int i=0;i<TAM;i++){
+        fscanf(arquivo, "%d\n", &vetor[i]); 
+    }
+    t = clock();
+    for(int i=0;i<TAM;i++){
+        inserir(vetor[i],&a);
+    }
+    t = clock() - t;
 	
-Hash *tabela; 				   //declarando um apontador para o tipo Hash
-tabela = inicializa_hash(TAM); //inicializando a tabela HASH
-dicionario* d; 				   //declarando um apontador para o tipo dicionario
-int x, y; 					   //variaveis de suporte para os loops do menu
-
-
-
-do{ // inicio do loop do menu
-	limpaTela();
-	printf("|1 adicionar valores a HASH\n"
-		   "|2 buscar uma chave na HASH\n"
-	       "|3 remover uma chave na HASH\n"
-		   "|4 quantidade de itens na HASH\n"
-		   "|0 sair\n"
-		   "|Opcao escolhida: ");
-	scanf("%d", &x);
-	switch(x){
-		case 1:			
-			do{
-				limpaTela();
-				printf("|Insira o valor da chave: ");
-				scanf("%d", &y);
-				insere_hash(tabela, d, y);
-				printf("|0 voltar: ");
-				scanf("%d", &y);
-			}while(y != 0);
-			break;
-		case 2:
-			do{
-				limpaTela();
-				printf("|Insira o valor da chave a ser buscada: ");
-				scanf("%d", &y);
-				busca_hash(tabela, y, d);
-				printf("|0 voltar: ");
-				scanf("%d", &y);
-			}while(y != 0);
-			break;
-		case 3:
-			do{
-				limpaTela();
-				limpaTela();
-				printf("|Insira o valor da chave a ser buscada: ");
-				scanf("%d", &y);
-				retira_hash(tabela, y, d);
-				
-				printf("|0 voltar: ");
-				scanf("%d", &y);
-			}while(y != 0);
-			break;
-		case 4: 
-			do{
-				limpaTela();
-				printf("|A quantidade eh: %d\n", tabela->quantidade);
-				printf("|0 voltar: ");
-				scanf("%d", &y);
-			}while(y != 0);
-			break;
-		default:			
-			break;
-	}
-}while(x != 0); 
-
-free(tabela); //liberando espaço de memória alocado para tabela
-free(d);      //liberando espaço de memória alocado para d
-
-return 0;
+    printf("Demorou %.3f segundos\n",((double)t)/((CLOCKS_PER_SEC)));
+    return 0;
 }
