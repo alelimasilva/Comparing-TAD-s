4dad47e47c046550b413f4376db29dd0e613954b
Alexandre Lima <64177769+alelimasilva@users.noreply.github.com>
2020-12-01 18:45:49 -0300

    Update hashLiEnc.h

--- hashLiEnc.h
+++ hashLiEnc.h
@@ -5,19 +5,17 @@
    #include <windows.h>
 #endif 
 
-#define TAM 100 //constante definida para determinar o tamanho da tabela HASH
-
-struct registro {
+struct registro{
 	int chave; 
 	struct registro *prox; 
 };
-typedef struct registro Celula; // Nó da hash
+typedef struct registro No;
 
 void limpaTela();//Limpa o terminal para melhorar a aparencia do programa
-void inicializa_hash(Celula **hash); // inicializa a hash
-int calcula_posicao(int chave); // calcula a posição da hash
-void insere_hash(int chave, Celula **hash); // verifica se pode ser inserido, se sim insere a chave na hash
-void imprime_hash(Celula **hash); // imprime toda a hash
-void remove_hash(int chave, Celula **hash); // apaga um dado da hash
-void imprime_listaenc(int pos, Celula **hash); // percorre uma lista encadeada em uma posição da hash e a imprime
-int buscalista(int chave, Celula **hash); // busca um dado em uma lista encadeada
+void inicializa_hash(No **hash, int tam); // inicializa a hash
+int calcula_posicao(unsigned int chave, int tam); // calcula a posição da hash
+int buscalista(unsigned int chave, No **hash, int tam); // busca um dado em uma lista encadeada
+void insere_hash(unsigned int chave, No **hash, int tam); // verifica se pode ser inserido, se sim insere a chave na hash
+void imprime_hash(No **hash, int tam); // imprime toda a hash
+void imprime_listaenc(int pos, No **hash); // percorre uma lista encadeada em uma posição da hash e a imprime
+void apaga_hash(unsigned int chave, No **hash, int tam); // apaga uma chave da hash
